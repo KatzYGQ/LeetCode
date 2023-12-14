5de0c67b739ed6a3f5609b2c412b1e62c4040d69
KatzYGQ <126522844+KatzYGQ@users.noreply.github.com>
2023-12-13 19:15:13 -0500

    Create solution.java

---
+++ 374. Guess Number Higher or Lower/solution.java
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int left=0;
+        int right=n;
+
+        while(left <=right){
+            int mid =left+(right-left)/2;
+
+            if(guess(mid) == -1){
+                right=mid-1;
+            } else if(guess(mid) == 1 ){
+                left=mid+1;
+            } else if(guess(mid) == 0){
+                return  mid;
+            }
+
+        }
+        return 0;
+    }
+}
+
+
+//Binary Search
