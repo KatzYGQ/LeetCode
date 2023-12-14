75a49ae16d54c3789365d1bfcf4368629ec2faff
KatzYGQ <126522844+KatzYGQ@users.noreply.github.com>
2023-12-13 19:15:51 -0500

    Create solution.java

---
+++ 278. First Bad Version/solution.java
@@ -0,0 +1,26 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left=0;
+        int right=n;
+        
+
+        while(left<=right){
+            int mid =left+(right-left)/2;
+            if(isBadVersion(mid)==true){
+               
+                right=mid-1;
+            }else if(isBadVersion(mid)==false){
+                 left=mid+1;
+            }
+
+        }
+
+        return left;
+    }
+}
+
+
+//Binary Search
