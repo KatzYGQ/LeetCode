43f188b4cb6d29f49bd1cb4200792e90e5aad049
ElioYuan <126522844+KatzYGQ@users.noreply.github.com>
2024-04-03 17:00:11 -0400

    Create Solution1.java

---
+++ 138. Copy List with Random Pointer/Solution1.java
@@ -0,0 +1,23 @@
+class Solution {
+    public Node copyRandomList(Node head) {
+        if(head == null) return null;
+        Node cur = head;
+        Map<Node, Node> map = new HashMap<>();
+        // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
+        while(cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+        cur = head;
+        // 4. 构建新链表的 next 和 random 指向
+        while(cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+            cur = cur.next;
+        }
+        // 5. 返回新链表的头节点
+        return map.get(head);
+    }
+}
+
+
