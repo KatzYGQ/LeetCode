3f9bc3d074ddede61caa51966bafe25b9f1fa805
ElioYuan <126522844+KatzYGQ@users.noreply.github.com>
2024-04-08 20:47:16 -0400

    Create solution.java

---
+++ Array/59. Spiral Matrix II/solution.java
@@ -0,0 +1,61 @@
+// 给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
+// 示例:
+
+// 输入: 3 输出: [ [ 1, 2, 3 ], [ 8, 9, 4 ], [ 7, 6, 5 ] ]
+
+
+// 而求解本题依然是要坚持循环不变量原则。
+
+// 模拟顺时针画矩阵的过程:
+
+// 填充上行从左到右
+// 填充右列从上到下
+// 填充下行从右到左
+// 填充左列从下到上
+// 由外向内一圈一圈这么画下去。
+
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] nums = new int[n][n];
+        int startX = 0, startY = 0;  // 每一圈的起始点
+        int offset = 1;
+        int count = 1;  // 矩阵中需要填写的数字
+        int loop = 1; // 记录当前的圈数
+        int i, j; // j 代表列, i 代表行;
+
+        while (loop <= n / 2) {
+
+            // 顶部
+            // 左闭右开，所以判断循环结束时， j 不能等于 n - offset
+            for (j = startY; j < n - offset; j++) {
+                nums[startX][j] = count++;
+            }
+
+            // 右列
+            // 左闭右开，所以判断循环结束时， i 不能等于 n - offset
+            for (i = startX; i < n - offset; i++) {
+                nums[i][j] = count++;
+            }
+
+            // 底部
+            // 左闭右开，所以判断循环结束时， j != startY
+            for (; j > startY; j--) {
+                nums[i][j] = count++;
+            }
+
+            // 左列
+            // 左闭右开，所以判断循环结束时， i != startX
+            for (; i > startX; i--) {
+                nums[i][j] = count++;
+            }
+            startX++;
+            startY++;
+            offset++;
+            loop++;
+        }
+        if (n % 2 == 1) { // n 为奇数时，单独处理矩阵中心的值
+            nums[startX][startY] = count;
+        }
+        return nums;
+    }
+}
