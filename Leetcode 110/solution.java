cc612d45e99c02fb31c21bee635a85817f2c7e3e
ElioYuan <126522844+KatzYGQ@users.noreply.github.com>
2024-04-24 15:08:20 -0400

    Create solution.java

---
+++ Leetcode 110/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        return Math.abs(depth(root.left) - depth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(depth(root.left), depth(root.right)) + 1;
+    }
+}
